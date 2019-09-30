fbc68adf9ea3a019401e1d4d46dd358c9bcdc1cc
Dmytro Lisitsyn <dmytro.lisitsyn@gmail.com>
2019-09-30 14:18:32 +0300

    Define NetworkError as protocol

--- Fotios/Network/NetworkError.swift
+++ Fotios/Network/NetworkError.swift
@@ -1,40 +1,31 @@
 //
-//  NetworkError.swift
 //  Fotios
 //
-//  Created by Dmytro Lisitsyn on 9/27/19.
-//  Copyright © 2019 Dmytro Lisitsyn. All rights reserved.
+//  Copyright (C) 2019 Dmytro Lisitsyn
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in all
+//  copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+//  SOFTWARE.
 //
 
 import Foundation
 
-public enum NetworkError: Error {
-    
-    case unauthorized
-    case badGateway
-    case notFound
-    case timeout
-    case cancelled
-    
-    case unknown(message: String)
-    
-    case unexpectedResponse(context: String)
-    case undefinedMapper(context: String)
-    case undefinedRequestFactory(context: String)
-    
-    init?(errorCode: Int) {
-        switch errorCode {
-        case 401:
-            self = .unauthorized
-        case 404:
-            self = .notFound
-        case 408:
-            self = .timeout
-        case 502:
-            self = .badGateway
-        default:
-            return nil
-        }
-    }
-        
+public protocol NetworkError: Error {
+
+    init(_ networkBody: Data, statusCode: Int) throws
+
 }
--- Fotios/Network/NetworkSession.swift
+++ Fotios/Network/NetworkSession.swift
@@ -1,14 +1,32 @@
 //
-//  NetworkSession.swift
 //  Fotios
 //
-//  Created by Dmytro Lisitsyn on 9/27/19.
-//  Copyright © 2019 Dmytro Lisitsyn. All rights reserved.
+//  Copyright (C) 2019 Dmytro Lisitsyn
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in all
+//  copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+//  SOFTWARE.
 //
 
 import UIKit
 
 public typealias NetworkSessionResponse = (data: Data?, meta: URLResponse?, error: Error?)
+
+// MARK: NetworkSession
 
 public protocol NetworkSession {
     
@@ -40,7 +58,9 @@ extension URLSession: NetworkSession {
     
 }
 
-public class MockNetworkSession {
+// MARK: MockNetworkSession
+
+public class MockNetworkSession: NetworkSession {
     
     public var data: Data?
     public var statusCode: Int
@@ -63,10 +83,6 @@ public class MockNetworkSession {
         self.statusCode = statusCode
         self.callbackError = callbackError
     }
-    
-}
-
-extension MockNetworkSession: NetworkSession {
     
     public func send(_ request: URLRequest, completionHandler: @escaping (NetworkSessionResponse) -> Void) -> NetworkTask {
         let url = request.url!
