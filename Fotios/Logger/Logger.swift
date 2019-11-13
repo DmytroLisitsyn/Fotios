633e96b127c282c861a56280c055c36bc0c833d3
Dmytro Lisitsyn <dl@madappgang.com>
2019-11-13 15:56:49 +0200

    Add Logger and AppEventsNotifier

---
+++ Fotios/Logger/Logger.swift
@@ -0,0 +1,77 @@
+//
+//  Fotios
+//
+//  Copyright (C) 2019 Dmytro Lisitsyn
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in all
+//  copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+//  SOFTWARE.
+//
+
+import Foundation
+
+public enum LoggerEvent {
+    
+    case info(String)
+    
+    case event(String)
+
+    case success(String)
+    
+    /// In `source` provide description of the failed action in present tense. For example "Authorize with session token".
+    case failure(source: String, error: Error)
+    
+}
+
+public protocol Logger: AnyObject {
+    
+    func log(_ event: LoggerEvent)
+    
+}
+
+extension Logger {
+    
+    func makeMessage(describing event: LoggerEvent) -> String {
+        let prefix = "➤ "
+        
+        switch event {
+        case .info(let message):
+            return "\(prefix)ℹ️ \(message)"
+        case .event(let message):
+            return "\(prefix)✴️ \(message)"
+        case .success(let message):
+            return "\(prefix)✅ \(message)"
+        case .failure(let source, let error):
+            var source = source
+            
+            if !source.isEmpty {
+                let first = Character("\(source.first!)".lowercased())
+                source.removeFirst()
+                source.insert(first, at: source.startIndex)
+            }
+            
+            var errorString = "\(error)"
+            
+            if let localizedError = error as? LocalizedError {
+                errorString = localizedError.errorDescription ?? localizedError.localizedDescription
+            }
+            
+            return "\(prefix)🛑 Error when \(source): \(errorString)"
+        }
+    }
+    
+}
---
+++ Fotios/Logger/LoggerBundle.swift
@@ -0,0 +1,46 @@
+//
+//  Fotios
+//
+//  Copyright (C) 2019 Dmytro Lisitsyn
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in all
+//  copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+//  SOFTWARE.
+//
+
+import Foundation
+
+public final class LoggerBundle {
+    
+    public let items: [Logger]
+    
+    public init(_ items: [Logger]) {
+        self.items = items
+        
+        let message = "Logger enabled. To disable, remove it from dependencies. Have a nice day! ✨"
+        log(.info(message))
+    }
+    
+}
+
+extension LoggerBundle: Logger {
+    
+    public func log(_ event: LoggerEvent) {
+        items.forEach { $0.log(event) }
+    }
+    
+}
