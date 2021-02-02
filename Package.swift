5a0ad2b96bf2fa58baf6a6ccf62b738a859d59bf
Dmytro Lisitsyn <dlisitsyn@pm.me>
2021-02-02 08:33:36 +0200

    Add Info.plist to ignored files

--- Package.swift
+++ Package.swift
@@ -5,23 +5,18 @@ import PackageDescription
 
 let package = Package(
     name: "Fotios",
-    platforms: [.iOS(.v13), .macOS(.v10_14)],
+    platforms: [.macOS(.v10_14), .iOS(.v13), .tvOS(.v13)],
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "Fotios",
             targets: ["Fotios"]),
     ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
-    ],
+    dependencies: [],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "Fotios",
-            dependencies: []),
+            dependencies: [],
+            exclude: ["Info.plist"]),
         .testTarget(
             name: "FotiosTests",
             dependencies: ["Fotios"]),
