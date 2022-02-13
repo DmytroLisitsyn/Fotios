b02b46eb913378a2c36d1a752fb38acfc9f267c2
Dmytro Lisitsyn <dlisitsyn@pm.me>
2022-02-13 04:25:55 +0200

    Add support for All Platforms

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "Fotios",
-    platforms: [.macOS(.v10_14), .iOS(.v13), .tvOS(.v13)],
+    platforms: [.macOS(.v10_15), .iOS(.v13), .watchOS(.v6), .tvOS(.v13)],
     products: [
         .library(
             name: "Fotios",
